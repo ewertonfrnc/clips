0ab41654b696b3438239de080a7a573b8758934e
Ewerton <ewefsilva@gmail.com>
2023-09-14 14:41:44 -0300

    feat: replaced missing files

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -4,18 +4,25 @@ import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
-  @Input() modalId = '';
+  @Input() modalID = ''
 
-  constructor(public modal: ModalService, public el: ElementRef) {}
+  constructor(
+    public modal: ModalService, 
+    public el: ElementRef
+  ) { 
+    
+  }
 
   ngOnInit(): void {
-    document.body.appendChild(this.el.nativeElement);
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
-    this.modal.toggleModal(this.modalId);
+    this.modal.toggleModal(this.modalID)
   }
+
 }
